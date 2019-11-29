19a3297377b80a4aa2fc97d9df4c99711d888ffc
icoldplayer <slashsdull@gmail.com>
2019-11-29 23:52:38 +0700

    update to python 3.7

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/icoldplayer/Documents/django/cpm/env"
+set -gx VIRTUAL_ENV "/home/icoldplayer/Documents/django/kasir/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
@@ -52,7 +52,7 @@ if test -z "$VIRTUAL_ENV_DISABLE_PROMPT"
         set -l old_status $status
 
         # Prompt override?
-        if test -n "(env) "            
+        if test -n "(env) "
             printf "%s%s" "(env) " (set_color normal)
         else
             # ...Otherwise, prepend env
--- env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/icoldplayer/Documents/django/cpm/env/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
